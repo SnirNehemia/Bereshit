a4940e4b4eee6196257095a71bcef35a3c80674e
SaarNehemia <saar.nehemia@gmail.com>
2025-03-01 12:30:50 +0200

    merged with main

--- config.py
+++ config.py
@@ -17,10 +17,17 @@ else:
 project_folder = Path(__file__).parent
 plt.rcParams['animation.ffmpeg_path'] = project_folder.joinpath(
     r'ffmpeg-2025-02-20-git-bc1a3bfd2c-essentials_build\bin\ffmpeg.exe')
+
+OUTPUT_FOLDER = project_folder.joinpath('outputs')
+OUTPUT_FOLDER.mkdir(exist_ok=True, parents=True)
 now = datetime.now()
 date_str = now.strftime('%d_%m_%Y')
+ANIMATION_FILEPATH = OUTPUT_FOLDER.joinpath(f"simulation_{date_str}.mp4")
+SPECIFIC_FIG_FILEPATH = OUTPUT_FOLDER.joinpath(f"specific_fig_{date_str}.png")
+STATISTICS_FIG_FILEPATH = OUTPUT_FOLDER.joinpath(f"statistics_fig_{date_str}.png")
 
 # --------------------------------------- CONFIG PARAMETERS -------------------------------------------------------- #
+np.random.seed = 0
 # general config params
 NOISE_STD = 0.5
 DT = 1.0  # time passing from frame to frame (relevant when calculating velocities)
@@ -43,8 +50,6 @@ EYES_PARAMS = ((np.radians(0), np.radians(60)))
 INPUT_SIZE = 2 + 2 + 3 * len(EYES_PARAMS) * 4
 # 2 for hunger and thirst, 2 for speed, 3 (flag, distance, angle) for each eye * 4 channels
 OUTPUT_SIZE = 2
-
-SAVE_FILENAME = f"simulation_{date_str}.mp4"
 
 # For food
 FOOD_DISTANCE_THRESHOLD = 50
--- simulation.py
+++ simulation.py
@@ -1,6 +1,8 @@
 # simulation.py
 import numpy as np
 from scipy.spatial import KDTree
+
+from brain import Brain
 from creature import Creature
 from environment import Environment
 from tqdm import tqdm
@@ -18,9 +20,20 @@ class Simulation:
     Implements multi-channel perception by using separate KDTree queries for each target type.
     """
 
-    def __init__(self, creatures: dict[Creature], environment: Environment):
-        self.creatures = creatures
-        self.env = environment
+    def __init__(self):
+        # Create the environment. Ensure that 'map.png' exists and follows the color conventions.
+        self.env = Environment(map_filename=config.ENV_PATH,
+                               grass_generation_rate=config.GRASS_GENERATION_RATE,
+                               leaves_generation_rate=config.LEAVES_GENERATION_RATE)
+
+        # Initialize creatures (ensuring they are not in forbidden areas).
+        self.creatures = self.initialize_creatures(num_creatures=config.NUM_CREATURES,
+                                                   simulation_space=config.SIMULATION_SPACE,
+                                                   input_size=config.INPUT_SIZE,
+                                                   output_size=config.OUTPUT_SIZE,
+                                                   eyes_params=config.EYES_PARAMS,
+                                                   env=self.env)
+
         # Build a KDTree for creature positions.
         self.creatures_kd_tree = self.build_creatures_kd_tree()
         self.max_creature_id = len(self.creatures.keys()) - 1
@@ -32,6 +45,65 @@ class Simulation:
         self.max_creature_energy_per_frame = []
         self.mean_creature_energy_per_frame = []
         self.std_creature_energy_per_frame = []
+
+    @staticmethod
+    def initialize_creatures(num_creatures, simulation_space, input_size, output_size,
+                             eyes_params, env: Environment):
+        """
+        Initializes creatures ensuring they are not placed in a forbidden (black) area.
+        """
+        creatures = dict()
+        for id in range(num_creatures):
+            position = []
+            valid_position = False
+            while not valid_position:
+                position = np.random.rand(2) * simulation_space
+                # Convert (x, y) to indices (col, row)
+                col, row = int(position[0]), int(position[1])
+                height, width = env.map_data.shape[:2]
+                # Check bounds and obstacle mask.
+                if col < 0 or col >= width or row < 0 or row >= height:
+                    continue
+                if env.obstacle_mask[row, col]:
+                    continue
+                valid_position = True
+
+            # static traits
+            max_age = 50
+            max_weight = 10.0
+            max_height = 5.0
+            max_speed = [5.0, 5.0]
+            color = np.random.rand(3)  # Random RGB color.
+
+            energy_efficiency = 1
+            speed_efficiency = 0.1
+            food_efficiency = 1
+            reproduction_energy = config.REPRODUCTION_ENERGY
+            max_energy = config.MAX_INIT_ENERGY
+
+            vision_limit = 100.0
+            brain = Brain([input_size, output_size])
+
+            # dynamic traits
+            weight = np.random.rand() * max_weight
+            height = np.random.rand() * max_height
+            speed = (np.random.rand(2) - 0.5) * max_speed
+            energy = np.random.rand() * max_energy
+            hunger = np.random.rand() * 10
+            thirst = np.random.rand() * 10
+
+            # init creature
+            creature = Creature(max_age=max_age, max_weight=max_weight, max_height=max_height, max_speed=max_speed,
+                                color=color,
+                                energy_efficiency=energy_efficiency, speed_efficiency=speed_efficiency,
+                                food_efficiency=food_efficiency, reproduction_energy=reproduction_energy,
+                                max_energy=max_energy,
+                                eyes_params=eyes_params, vision_limit=vision_limit, brain=brain,
+                                weight=weight, height=height,
+                                position=position, speed=speed, energy=energy, hunger=hunger, thirst=thirst)
+
+            creatures[id] = creature
+        return creatures
 
     def build_creatures_kd_tree(self) -> KDTree:
         """
@@ -336,9 +408,7 @@ class Simulation:
 
             if np.min(food_distances) <= config.FOOD_DISTANCE_THRESHOLD:
                 # update creature energy
-                print(f'creature {id=} found {food_type} and has {creature.energy}.')
                 creature.energy += creature.food_efficiency * food_energy
-                print(f'creature {id=} ate {food_type} and has {creature.energy}.')
 
                 # remove food from board
                 closest_food_point = self.env.grass_points[np.argmin(food_distances)]
@@ -363,7 +433,6 @@ class Simulation:
         dt = config.DT
         noise_std = config.NOISE_STD
         num_frames = config.NUM_FRAMES
-        save_filename = config.SAVE_FILENAME
 
         # Initialize the progress bar outside of the update function
         progress_bar = tqdm(total=num_frames, desc="Simulation progress")
@@ -405,6 +474,7 @@ class Simulation:
         # -------------------------------- function for simulation progress -------------------------------- #
 
         print('starting simulation')
+
         def update(frame):
 
             # --------------------------- run frame --------------------------- #
@@ -488,9 +558,10 @@ class Simulation:
 
         # ----------------------------------- run simulation and save animation ------------------------------------ #
         ani = animation.FuncAnimation(fig, update, frames=num_frames, interval=50, blit=True)
-        ani.save(save_filename, writer="ffmpeg", dpi=200)
+        ani.save(config.ANIMATION_FILEPATH, writer="ffmpeg", dpi=200)
         plt.close(fig)
-        print('finished simulation')
+        print('finished simulation.')
+        print(f'Simulation animation saved as {config.ANIMATION_FILEPATH.stem}.')
 
         # ----------------------------------- Plot graphs after simulation ended ----------------------------------- #
         # creature energy fig
@@ -504,6 +575,8 @@ class Simulation:
         plt.title('creature energy per frame')
         plt.ylabel('creature energy')
         plt.xlabel('frame number')
+        plt.savefig(fname=config.SPECIFIC_FIG_FILEPATH)
+        print(f'specific fig saved as {config.SPECIFIC_FIG_FILEPATH.stem}.')
 
         # Final fig
         fig, ax = plt.subplots(2, 1, sharex='all')
@@ -519,4 +592,7 @@ class Simulation:
         ax[1].set_title('energy statistics per frame')
         ax[1].set_xlabel('frame number')
         ax[1].legend()
+        fig.savefig(fname=config.STATISTICS_FIG_FILEPATH)
+        print(f'statistics fig saved as {config.STATISTICS_FIG_FILEPATH.stem}.')
+
         plt.show()

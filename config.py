e2b94ac208f15c7afe5de034504203258b1cdcfb
SnirNehemia <112329287+SnirNehemia@users.noreply.github.com>
2025-03-31 22:17:50 +0300

    initial rebalance
    it seems to work better now

--- config.py
+++ config.py
@@ -25,16 +25,16 @@ np.random.seed = 0
 class Config:
     # Simulation parameters
     DT = 2.0  # time passing from frame to frame (relevant when calculating velocities)
-    NUM_FRAMES = 200  # the actual number of steps = NUM_FRAMES * UPDATE_ANIMATION_INTERVAL
-    UPDATE_ANIMATION_INTERVAL = 30  # 30  # update the animation every n steps
+    NUM_FRAMES = 600  # the actual number of steps = NUM_FRAMES * UPDATE_ANIMATION_INTERVAL
+    UPDATE_ANIMATION_INTERVAL = 35  # 30  # update the animation every n steps
     FRAME_INTERVAL = 75  # interval between frames in animation [in ms]
     STATUS_EVERY_STEP = True  # choose if to update every step or every frame
-    UPDATE_KDTREE_INTERVAL = 30  # update the kdtree every n steps
+    UPDATE_KDTREE_INTERVAL = 90  # update the kdtree every n steps
     DEBUG_MODE = False  # breakpoint in update_func after each frame (not step!)
     STUCK_PERCENTAGE = 0.75  # percentage of MAX_NUM_CREATURES to stuck simulation
 
     # Purge parameters
-    DO_PURGE = False
+    DO_PURGE = True
     PURGE_PERCENTAGE = 0.1  # percentage out of MAX_NUM_CREATURES to do purge
     PURGE_SPEED_THRESHOLD = 1  # if the creature's speed is below this threshold at first reproduction, it will be removed
     PURGE_FRAME_FREQUENCY = 50  # do purge every PURGE_FRAME_FREQUENCY frames
@@ -42,9 +42,9 @@ class Config:
     # Environment parameters
     ENV_PATH = r"Penvs\Env8.png"
     BOUNDARY_CONDITION = 'zero'  # what to do with the velocity on the boundaries - 'mirror' or 'zero'
-    MAX_GRASS_NUM = 60
-    GRASS_GENERATION_RATE = 10  # if exceeding MAX_GRASS_NUM replace GRASS_GENERATION_RATE grass points
-    GRASS_GROWTH_CHANCE = 0.5  # maybe will be useful to create droughts
+    MAX_GRASS_NUM = 50
+    GRASS_GENERATION_RATE = 2  # if exceeding MAX_GRASS_NUM replace GRASS_GENERATION_RATE grass points
+    GRASS_GROWTH_CHANCE = 0.1  # maybe will be useful to create droughts
     MAX_LEAVES_NUM = 50
     LEAVES_GENERATION_RATE = 0  # if exceeding MAX_LEAVES_NUM replace LEAVES_GENERATION_RATE grass points
     LEAVES_GROWTH_CHANCE = 0.5  # maybe will be useful to create droughts
@@ -57,8 +57,8 @@ class Config:
     # Energy parameters
     INIT_MAX_ENERGY = 20000  # maybe useful for maturity test before reproduction
     REPRODUCTION_ENERGY = 12000  # energy cost of reproduction
-    MIN_LIFE_ENERGY = 3000  # energy to be left after reproduction
-    GRASS_ENERGY = 10000
+    MIN_LIFE_ENERGY = 5000  # energy to be left after reproduction
+    GRASS_ENERGY = 5000
     LEAF_ENERGY = 2000
     INIT_DIGEST_DICT = {'grass': 1, 'leaf': 0.5, 'creature': 0}
 
@@ -66,10 +66,10 @@ class Config:
         assert 0 <= c_digest <= 1, 'Config Error: INIT_DIGEST_DICT is not set correctly (values between 0-1).'
 
     # Creatures parameters
-    NUM_CREATURES = 500  # init size of population
+    NUM_CREATURES = 800  # init size of population
     MAX_NUM_CREATURES = 1250
     INIT_MAX_AGE = 4000  # [steps]
-    INIT_MAX_MASS = 5  # [kg]
+    INIT_MAX_MASS = 10  # [kg]
     INIT_MAX_HEIGHT = 0.5  # [m]
     INIT_MAX_STRENGTH = 50  # [N]
     MAX_SPEED = 2.5  # [m/sec] maximum speed of the creature
--- physical_model.py
+++ physical_model.py
@@ -9,23 +9,23 @@ class PhysicalModel:
     g: float = 10  # [m/sec^2]
 
     # drag force (air resistence)
-    gamma: float = 0.1  # [F]/[v] = [kg/sec] linear drag air resistence (dominant in low speeds)
-    c_drag: float = 0.1  # [F]/[v^2] quadratic drag air resistence (dominant in high speeds)
+    gamma: float = 1e-5  # [F]/[v] = [kg/sec] linear drag air resistence (dominant in low speeds)
+    c_drag: float = 1e-7  # [F]/[v^2] quadratic drag air resistence (dominant in high speeds)
 
     # friction force
-    mu_static: float = 0.5  # [no units]  # higher mu_static means faster movement
-    mu_kinetic: float = 0.3  # [no units]
+    mu_static: float = 1.5  # [no units]  # higher mu_static means faster movement
+    mu_kinetic: float = 0.5  # [no units]
     alpha_mu: float = 1  # [no units]
     assert mu_static > mu_kinetic, f'Physical model error: {mu_static=} must be larger than {mu_kinetic=}'
 
     # convert physical parameters to energy
     energy_conversion_factors = {
-        'activity_efficiency': 0.4,  #0.25,  # propulsion force to energy factor (higher -> efficient)
-        'heat_loss': 0.1,  # propulsion force to wasted energy (higher -> more loss)
-        'rest': 0.2,  # constant for Basal Metabolic Rate (BMR) energy
-        'digest': 0.3,  # [E]/[no units] convert digest factor to digest energy
-        'height': 5,  # [E]/[h]  # convert height to height energy
-        'mass': 3  # [E]/[m] = [v^2] = [m^2/sec^4]  convert mass to mass energy
+        'activity_efficiency': 0.5,  #0.25,  # propulsion force to energy factor (higher -> efficient)
+        'heat_loss': 0.01,  # propulsion force to wasted energy (higher -> more loss)
+        'rest': 0.1,  # constant for Basal Metabolic Rate (BMR) energy
+        'digest': 0.05,  # [E]/[no units] convert digest factor to digest energy
+        'height': 0.05,  # [E]/[h]  # convert height to height energy
+        'mass': 0.05  # [E]/[m] = [v^2] = [m^2/sec^4]  convert mass to mass energy
     }
 
     # trait change formulas based on given energy

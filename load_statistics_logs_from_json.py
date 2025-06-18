c6844ce61bdac165b7ccee6a73f3aade6536d31e
SnirNehemia <112329287+SnirNehemia@users.noreply.github.com>
2025-06-18 22:37:45 +0300

    Update load_statistics_logs_from_json.py
    Removed config dependency

--- load_statistics_logs_from_json.py
+++ load_statistics_logs_from_json.py
@@ -1,14 +1,3 @@
-import json
-from input.codes import config, physical_model
-
-# Load config
-config_yaml_relative_path = r"input\yamls\2025_04_18_config.yaml"
-config = config.load_config(yaml_relative_path=config_yaml_relative_path)
-
-# Load physical model
-physical_model_yaml_relative_path = r"input\yamls\2025_04_18_physical_model.yaml"
-physical_model = physical_model.load_physical_model(yaml_relative_path=physical_model_yaml_relative_path)
-
 from statistics_logs import StatisticsLogs
 
 b = StatisticsLogs.from_json(
